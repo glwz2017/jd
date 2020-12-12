ce1ed886ceb974f50dbb24479bfd0f9a95f6ee29
glwz2017 <2991703821@qq.com>
2020-12-12 16:40:40 +0800

    提交程序框架文件

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    {{ data }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      data: '我是首页999'
+    }
+  },
+  created () { },
+  mounted () { }
+}
+</script>
+<style lang="less" scoped>
+@import "../css/element.less";
+@colorPink: pink;
+
+.home {
+  color: @colorPink;
+  font-size: 96px;
+}
+</style>
